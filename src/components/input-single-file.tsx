7f5461fa151ed5a7648609f2556068716acd34af
Gustavo Costa <guccosta@gmail.com>
2026-01-20 16:25:47 -0300

    ajustando botao file

--- src/components/input-single-file.tsx
+++ src/components/input-single-file.tsx
@@ -1,13 +1,47 @@
 import { type VariantProps, tv } from "tailwind-variants";
-
+import Icon from "./icon";
+import Text from "./text";
+import UploadFileIcon from "../assets/icons/upload-file.svg?react";
+import FileImageIcon from "../assets/icons/image.svg?react";
 
 export const inputSingleFileVariants = tv({
   base: `
     flex flex-col items-center justify-center w-full
-  `
-})
+    border border-solid border-border-primary
+    group-hover:border-border-active
+    rounded-lg gap-1 transition
+  `,
 
-export default function InputSingleFile() {
+  variants: {
+    size: {
+      md: "px-5 py-6",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+export const inputSingleFileIconVariants = tv({
+  base: "fill-placeholder",
+  variants: {
+    size: {
+      md: "w-8 h-8",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+interface InputSingleFileProps
+  extends
+    VariantProps<typeof inputSingleFileVariants>,
+    React.ComponentProps<"input"> {
+  error?: React.ReactNode;
+}
+
+export default function InputSingleFile({ size, error }: InputSingleFileProps) {
   return (
     <div>
       <div className="w-full relative group cursor-pointer">
@@ -18,9 +52,26 @@ export default function InputSingleFile() {
                 opacity-0 cursor-pointer
                 `}
         />
-        <div className={inputSingleFileVariants()}>
-
+        <div className={inputSingleFileVariants({ size })}>
+          <Icon
+            svg={UploadFileIcon}
+            className={inputSingleFileIconVariants({ size })}
+          />
+          <Text variant="label-medium" className="text-placeholder text-center">
+            Arraste o arquivo aqui
+            <br />
+            ou clique para selecionar
+          </Text>
         </div>
+      </div>
+      {error && (
+        <Text variant="label-small" className="text-accent-red">
+          erro no campo
+        </Text>
+      )}
+
+      <div className="flex gap-3 items-center">
+        <Icon svg={FileImageIcon} />
       </div>
     </div>
   );
